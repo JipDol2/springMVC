2d3c00d2ae376be2b8150c52c3f37382c230c58d
seongju lee <gch03915@naver.com>
2022-12-18 16:43:53 +0900

    20221218 : build.gradle modify(:because of jstl) and jsp<->servlet and mvc model use

---
+++ src/main/java/jipdol2/springmvc/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,29 @@
+package jipdol2.springmvc.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jipdol2.springmvc.domain.member.Member;
+import jipdol2.springmvc.domain.member.MemberRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet",urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members",members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+}
